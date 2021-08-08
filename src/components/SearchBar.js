ae50abe07fa2451025bfb18680d36f08237b0c4b
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-08 21:16:14 +0100

    Transformed input from uncontrolled to controlled

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: 'Hi there!' };
+
   render() {
     return (
       <div className='ui segment'>
         <form className='ui form'>
           <div className='field'>
             <label>Image Search</label>
-            <input type='text' onChange={this.onInputChange}></input>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
           </div>
         </form>
       </div>

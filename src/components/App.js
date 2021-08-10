b0f83b4aadc5304356d4c0b66eb32a6536ba6b74
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-10 20:20:12 +0100

    implemented search method with unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID F-sqsxbrI7jZnVbQAsEFwmzo77BocH8fRmvAkW95-og',
+      },
+    });
   }
 
   render() {

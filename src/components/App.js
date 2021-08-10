09e90b344a5841ac9691e234eb71696b11643db0
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-10 21:48:43 +0100

    implemented async/await method to get the response variable

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID F-sqsxbrI7jZnVbQAsEFwmzo77BocH8fRmvAkW95-og',
       },
     });
+
+    console.log(response.data.results);
   }
 
   render() {

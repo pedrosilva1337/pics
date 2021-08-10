966a2338c749a6bfbb86e69c9e9cc2bd886faec4
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-10 22:15:27 +0100

    created ImageList file and passed the props to it

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] }; //empty array
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+    const response = await unsplash.get('/search/photos', {
       params: { query: term },
-      headers: {
-        Authorization: 'Client-ID F-sqsxbrI7jZnVbQAsEFwmzo77BocH8fRmvAkW95-og',
-      },
     });
 
     this.setState({ images: response.data.results });
@@ -20,7 +18,7 @@ class App extends React.Component {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images.
+        <ImageList images={this.state.images} />
       </div>
     );
   }

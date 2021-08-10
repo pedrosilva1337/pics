98205582f423afe0b8d0d1042f188132ada29abf
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-10 22:29:54 +0100

    looping the images vector and getting the images url

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,6 +4,8 @@ const ImageList = (props) => {
   const images = props.images.map((image) => {
     return <img src={image.urls.regular}></img>;
   });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;

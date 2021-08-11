36a1c6e08b02780be3c3f3bce480254a451590f7
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-11 19:55:56 +0100

    fixed grid problem and pictures are more evenly distributed with a dynamic span

---
+++ src/components/ImageCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10); // 150 porque é a altura que ficou definida na propriedade grid-auto-rows na imageList.css
+
+    this.setState({ spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular}></img>
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
